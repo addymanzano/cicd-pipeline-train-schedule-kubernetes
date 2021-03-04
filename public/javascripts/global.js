ec74398f31074a443999b2262890f488baecb013
Adam Manzano <adam.n.manzano@gmail.com>
2021-03-03 16:35:15 -0800

    changes for demo

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -24,20 +24,20 @@ function populateTrains() {
 function showTrainInfo(event) {
 	event.preventDefault();
 	var train = trains[$(this).attr('rel')];
-	var tableContent = '<table class="table"><tr><th>Station</th></tr>';
+	var tableContent = '<table class="table"><tr><th>Teacher</th></tr>';
 	$.each(train.stops, function(index){
       tableContent += '<tr>';
       tableContent += '<td><strong>' + this.station + '</strong></td>';
       var onTime = (this.status == 'ON-TIME');
       tableContent += '<td style="color: ' + ((onTime) ? 'green' : 'red') + '">' + this.status + '</td>';
-      tableContent += '<td>Arrives ';
+      tableContent += '<td>Starts ';
       if (onTime || this.arrival == this.delayedArrival) {
         tableContent += this.arrival;
       } else {
         tableContent += '<span style="text-decoration: line-through">' + this.arrival + '</span> ' + this.delayedArrival;
       }
       tableContent += '</td>';
-      tableContent += '<td>Departs ';
+      tableContent += '<td>Ends ';
       if (onTime || this.departure == this.delayedDeparture) {
         tableContent += this.departure;
       } else {
